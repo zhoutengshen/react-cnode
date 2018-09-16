693e64f964d8e3c69d278793f9e872b8fb874293
zhoutengshen <1724847624@qq.com>
2018-09-16 23:21:45 +0800

    添加serve-favico中间件 为网站添加网站图标

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const serverRender = require("react-dom/server");//服务器渲染
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const serverRender = require('react-dom/server');//服务器渲染
+const serveFavicon = require('serve-favicon'); // 网站图标中间件
+
 let app = express();
-const isDev = process.env.NODE_ENV === "development";
+app.use(serveFavicon(path.join(__dirname, "/favicon.ico")));// 注册网站图标中间价
+const isDev = process.env.NODE_ENV === 'development';
 if (!isDev) {//非开发环境
-    const serverEntry = require("../build/server.entry").default;
-    app.use("/public", express.static(path.join(__dirname, "../build")));
-    let indexTempateHtml = fs.readFileSync(path.join(__dirname, "../build/index.html"), "utf-8");
-    app.get("*", (req, resp) => {
+    const serverEntry = require('../build/server.entry').default;
+    app.use('/public', express.static(path.join(__dirname, '../build')));
+    let indexTempateHtml = fs.readFileSync(path.join(__dirname, '../build/index.html'), 'utf-8');
+    app.get('*', (req, resp) => {
         let renderHtml = serverRender.renderToString(serverEntry);
-        renderHtml = indexTempateHtml.replace("<!--app-->", renderHtml);
+        renderHtml = indexTempateHtml.replace('<!--app-->', renderHtml);
         resp.send(renderHtml);
     });
 
 } else {
-    const devStatic = require("../utils/devStatic");
+    const devStatic = require('../utils/devStatic');
     devStatic(app);
 }
 app.listen(3333, () => {
-    console.log("server listening at http://localhost:3333")
+    console.log('server listening at http://localhost:3333')
 });
